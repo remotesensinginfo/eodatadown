9d98bdb1068ed8d52e6c17057f2d265d81650ef2
Pete Bunting <petebunting@mac.com>
2020-06-01 14:42:23 +0100

    updated version - should have been in last commit.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ from distutils.core import setup
 import os
 
 setup(name='EODataDown',
-    version='2.2.20',
+    version='2.2.21',
     description='A tool for automating Earth Observation Data Downloading.',
     author='Pete Bunting',
     author_email='pfb@aber.ac.uk',
